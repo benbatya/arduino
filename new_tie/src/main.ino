c7aa6c3c1ab73a1bf9b99f424ed1acddbfb5e60d
Benjamin Batya <benjaminbatya@gmail.com>
2016-02-02 15:47:12 -0800

    Added the VU mode

--- new_tie/src/main.ino
+++ new_tie/src/main.ino
@@ -37,8 +37,9 @@ enum class MODE : byte
 {
     AMP = 0,
     FFT = 1,
+    VU = 2,
 
-    LEN = FFT+1
+    LEN
 };
 
 static MODE current_mode = MODE::FFT;
@@ -84,6 +85,9 @@ void setup()
             current_mode = MODE::FFT;
             break;
         case MODE::FFT:
+            current_mode = MODE::VU;
+            break;
+        case MODE::VU:
             current_mode = MODE::AMP;
             break;
         default:
@@ -102,6 +106,9 @@ void setup()
     case MODE::FFT:
         strip.setPixelColor(0, 0, 255, 0);
         break;
+    case MODE::VU:
+        strip.setPixelColor(0, 0, 0, 255);
+        break;
     default:
         break;
     }
@@ -119,6 +126,7 @@ void setup()
 }
 
 void flow_modes();
+void vu_mode();
 
 void loop() 
 {   
@@ -128,8 +136,11 @@ void loop()
     case MODE::AMP:
         flow_modes(); 
         break;
+    case MODE::VU:
+        vu_mode();
+        break;
     default:
-        return;
+        break;
     }
 }
 
@@ -371,4 +382,174 @@ uint32_t amp_to_color()
 }
 
 // start MODE::VU code
+// NOTE: taken from Adafruit's original tie code
+#define SAMPLE_WINDOW   10  // Sample window for average level
+#define PEAK_HANG 24 //Time of pause before peak dot falls
+#define PEAK_FALL 4 //Rate of falling peak dot
+#define INPUT_FLOOR 22 //Lower range of analogRead input
+#define INPUT_CEILING 300 //Max range of analogRead input, the lower the value the more sensitive (1023 = max)
 
+byte peak = 16;      // Peak level of column; used for falling dots
+//unsigned int sample;
+
+byte dotCount = 0;  //Frame counter for peak dot
+byte dotHangCount = 0; //Frame counter for holding peak dot
+
+void drawLine(uint8_t from, uint8_t to, uint32_t c);
+float fscale(float originalMin, float originalMax, float newBegin, float
+             newEnd, float inputValue, float curve);
+
+void vu_mode()
+{
+    float peakToPeak = amplitude();
+
+    //Fill the strip with rainbow gradient
+    for (uint16_t i = 0; i <= strip.numPixels() - 1; i++) 
+    {
+        strip.setPixelColor(i, Wheel(map(i, 0, strip.numPixels() - 1, 30, 150)));
+    }
+    
+    
+    //Scale the input logarithmically instead of linearly
+    uint16_t c = fscale(INPUT_FLOOR, INPUT_CEILING, strip.numPixels(), 0, peakToPeak, 2); 
+    
+    if (c < peak) 
+    {
+        peak = c;        // Keep dot on top
+        dotHangCount = 0;    // make the dot hang before falling
+    }
+    if (c <= strip.numPixels()) // Fill partial column with off pixels
+    {
+        drawLine(strip.numPixels(), strip.numPixels() - c, strip.Color(0, 0, 0));
+    }
+    
+    // Set the peak dot to match the rainbow gradient
+    uint16_t y = strip.numPixels() - peak; 
+    
+    strip.setPixelColor(y - 1, Wheel(map(y, 0, strip.numPixels() - 1, 30, 150))); 
+    
+    strip.show(); 
+    
+    // Frame based peak dot animation
+    if (dotHangCount > PEAK_HANG) //Peak pause length
+    {
+        if (++dotCount >= PEAK_FALL) //Fall rate 
+        {
+            peak++; 
+            dotCount = 0;
+        }
+    } 
+    else 
+    {
+        dotHangCount++; 
+    }
+}
+
+//Used to draw a line between two points of a given color
+void drawLine(uint8_t from, uint8_t to, uint32_t c) 
+{ 
+    uint8_t fromTemp; 
+    if (from > to) 
+    {
+        fromTemp = from; 
+        from = to; 
+        to = fromTemp;
+    }
+    for (int i = from; i <= to; i++) 
+    {
+        strip.setPixelColor(i, c);
+    }
+}
+
+
+float fscale(float originalMin, float originalMax, float newBegin, float
+             newEnd, float inputValue, float curve) 
+{ 
+    
+    float OriginalRange = 0; 
+    float NewRange = 0; 
+    float zeroRefCurVal = 0; 
+    float normalizedCurVal = 0; 
+    float rangedValue = 0; 
+    boolean invFlag = 0; 
+    
+    
+    // condition curve parameter
+    // limit range
+    
+    if (curve > 10) curve = 10; 
+    if (curve < -10) curve = -10; 
+    
+    curve = (curve * -.1); // - invert and scale - this seems more intuitive - postive numbers give more weight to high end on output 
+    curve = pow(10, curve); // convert linear scale into lograthimic exponent for other pow function
+    
+    /*
+     Serial.println(curve * 100, DEC);   // multply by 100 to preserve resolution  
+     Serial.println(); 
+     */
+    
+    // Check for out of range inputValues
+    if (inputValue < originalMin) 
+    {
+        inputValue = originalMin;
+    }
+    if (inputValue > originalMax) 
+    {
+        inputValue = originalMax;
+    }
+    
+    // Zero Refference the values
+    OriginalRange = originalMax - originalMin; 
+    
+    if (newEnd > newBegin) 
+    {
+        NewRange = newEnd - newBegin;
+    }
+    else 
+    {
+        NewRange = newBegin - newEnd; 
+        invFlag = 1;
+    }
+    
+    zeroRefCurVal = inputValue - originalMin; 
+    normalizedCurVal  =  zeroRefCurVal / OriginalRange;   // normalize to 0 - 1 float
+    
+    // Check for originalMin > originalMax  - the math for all other cases i.e. negative numbers seems to work out fine 
+    if (originalMin > originalMax) 
+    {
+        return 0;
+    }
+    
+    if (invFlag == 0) 
+    {
+        rangedValue =  (pow(normalizedCurVal, curve) * NewRange) + newBegin;
+        
+    }
+    else     // invert the ranges
+    {
+        rangedValue =  newBegin - (pow(normalizedCurVal, curve) * NewRange); 
+    }
+    
+    return rangedValue;
+}
+
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) 
+{ 
+    if (WheelPos < 85) 
+    {
+        return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+    } 
+    else if (WheelPos < 170) 
+    {
+        WheelPos -= 85; 
+        return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    } 
+    else 
+    {
+        WheelPos -= 170; 
+        return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    }
+}
